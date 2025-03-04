275adb9483c99531be7185fc61587d1c8cf11228
QuvonchbekCoder05 <166432175+QuvonchbekCoder05@users.noreply.github.com>
2025-03-04 09:24:10 +0500

    Fixed PostgreSQL host issue for Railway

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,15 +87,20 @@ ASGI_APPLICATION = "config.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "railway",  # Railway PostgreSQL database nomi
+#         "USER": "postgres",  # Railway foydalanuvchi nomi
+#         "PASSWORD": "qnydvhZgjOwqixpUWnguUADBlaZtKWRL",  # Railway paroli
+#         "HOST": "postgres.railway.internal",  # Railway serveri
+#         "PORT": "5432",  # PostgreSQL porti (odatiy: 5432)
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "railway",  # Railway PostgreSQL database nomi
-        "USER": "postgres",  # Railway foydalanuvchi nomi
-        "PASSWORD": "qnydvhZgjOwqixpUWnguUADBlaZtKWRL",  # Railway paroli
-        "HOST": "postgres.railway.internal",  # Railway serveri
-        "PORT": "5432",  # PostgreSQL porti (odatiy: 5432)
-    }
+    "default": dj_database_url.config(
+        default="postgres://postgres:qnydvhZgjOwqixpUWnguUADBlaZtKWRL@postgres.railway.internal:5432/railway"
+    )
 }
 
 
